6aa09e4c7141adc4415e212ccccb2cf59fce3244
nfproperty2069 <nfproperty2069@gmail.com>
2016-08-07 09:02:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-var opbeat = require('opbeat').start()
+var opbeat = require('opbeat').start();
 
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(opbeat.middleware.express())
+app.use(opbeat.middleware.express());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+var p;
+app.get('/', function(request, response, next) {
+    var count;
+    fs.readFile('stats', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        count = data;
+
+        count++;
+        p = count;
+        fs.writeFile("stats",p, function (err) {
+            if(err) {
+                return console.log(err);
+            }
+        } );
+    });
+    response.render('pages/index', {
+        cnt: p
+    });
+});
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
