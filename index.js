7540270851786fb419cb0151219f55e3bad5a31f
deepakpahadua <deepakpahadua@gmail.com>
2016-08-22 10:54:45 +0530

    deep

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 var p;
 app.get('/', function(request, response) {
     var count;
-    console.log('client ip : '+request.rawHeaders[1]);
+    console.log('client ip : '+request.connection.remoteAddress);
     fs.readFile('stats', 'utf8', function (err,data) {
         if (err) {
             return console.log(err);
