dc9f17e1a1f330ce0b2af2f6afe4a846c491c77f
deepakpahadua <deepakpahadua@gmail.com>
2016-08-22 10:11:34 +0530

    deep

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var opbeat = require('opbeat').start();
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var circularJSON = require('circular-json');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,7 +16,7 @@ app.set('view engine', 'ejs');
 var p;
 app.get('/', function(request, response) {
     var count;
-    console.log('client ip : '+request.ip);
+    console.log('client ip : '+request.headers.host);
     fs.readFile('stats', 'utf8', function (err,data) {
         if (err) {
             return console.log(err);
